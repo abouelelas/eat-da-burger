e16167ebe3711628f4df8e9fda890bd3ea34b3fa
Sarah Abouelela <abouelelas@gmail.com>
2020-08-29 12:47:57 -0400

    updated mysql connection

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-var jawsSql = process.env.JAWSDB_URL;
+
 var connection = null;
-if (typeof jawsSql === 'undefined' || jawsSql.length === 0){
-  connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "rootpassword",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
-  connection = mysql.createConnection(jawsSql);
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rootpassword",
+    database: "burgers_db"
+  });
+  
 }
 console.log("Test");
 console.log(jawsSql)
