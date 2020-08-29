f775f88d0b2b67d424515c01aae060b2b0f51155
Sarah Abouelela <abouelelas@gmail.com>
2020-08-29 09:58:33 -0400

    updated column names

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,7 +2,7 @@
 
 INSERT INTO burgers (burger_name) VALUES ('Doublecheese');
 INSERT INTO burgers (burger_name) VALUES ('Sara-tonin');
-INSERT INTO burgers (burger_name, devour) VALUES ('Cheese', true);
-INSERT INTO burgers (burger_name, devour) VALUES ('Ultimate bbq', true);
-INSERT INTO burgers (burger_name, devour) VALUES ('Big Mac', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ultimate bbq', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', true);
 INSERT INTO burgers (burger_name) VALUES ('Juicy Lucy');
